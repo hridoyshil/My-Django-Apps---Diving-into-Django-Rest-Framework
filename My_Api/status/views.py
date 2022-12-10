237e473ea6dfaa007fb50067072634e15d302bac
Hridoy <hriodykumarshil12@gmail.com>
2022-12-10 22:51:32 +0600

    Detail, Delete, Update API View
    https://rapidapi.com/blog/put-vs-patch/

--- My_Api/status/views.py
+++ My_Api/status/views.py
@@ -17,10 +17,31 @@ class StatusAPIView(APIView):
 
 
 class StatusListAPIView(generics.ListAPIView):
-    queryset = Status.objects.all()
+    queryset = Status.objects.all() 
     serializer_class = StatusSerializer
 
 
 class StatusCreateAPIView(generics.CreateAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer  
+
+class StatusDetailAPIView(generics.RetrieveAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = "id"
+
+
+    # def get_object(self, *args, **kwargs):
+    #     kwargs = self.kwargs
+    #     kw_id = kwargs.get("id")
+    #     return Status.objects.get(id=kw_id)
+
+class StatusUpdateAPIView(generics.UpdateAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = "id"
+
+class StatusDeleteAPIView(generics.DestroyAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = "id"
