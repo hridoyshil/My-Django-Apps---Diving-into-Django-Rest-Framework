c46e59c891002f6227f1f5c923348fd8ad14f7bd
Hridoy <hriodykumarshil12@gmail.com>
2022-12-11 20:42:20 +0600

    Using ViewSets and Router--

--- My_Api/status/urls.py
+++ My_Api/status/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
-from .views import (
-    StatusDetailUpdateDeleteView,
-    StatusListCreateView,
-)
+from .views import StatusViewset
+from rest_framework.routers import DefaultRouter
+
+
+# from .views import (StatusDetailUpdateDeleteView,StatusListCreateView)
 
 # status/ -> List, Create => GET, POST
 # status/<id>/ -> Details, Delete, Update => GET, DELETE, PUT/PATCH
 
-urlpatterns = [
-    path("status/", StatusListCreateView.as_view()),
-    path("status/<id>/", StatusDetailUpdateDeleteView.as_view()),
-]
+# urlpatterns = [
+#     path("status/", StatusListCreateView.as_view()),
+#     path("status/<id>/", StatusDetailUpdateDeleteView.as_view()),
+# ]
+
+router = DefaultRouter()
+router.register(r"status", StatusViewset, basename="status")
+
+urlpatterns = [] + router.urls
